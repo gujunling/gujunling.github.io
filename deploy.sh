dfadff486e8f64afe90d435fbb0d049dcb1fcb56
gujunling <gujunq1998@163.com>
2021-12-14 17:11:16 +0800

    ci(cicd): 修改 github action 的配置

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'blog.sweetheartjq.com' > CNAME
+echo 'gujunling.github.io' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:gujunling/myweb-frontend.git
