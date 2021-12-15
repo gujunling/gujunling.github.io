5e5dd36fbb686c2b8255ca4062b3b81d26a3f6a5
谷俊崎 <260480@gree.com.cn>
2021-12-15 17:36:21 +0800

    fix(评论系统): 保留阿里云域名的评论系统的clientID,解决在不同域名时部署导致评论系统不能用的问题，注意在使用不同域名时需要更换这两个值

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -6,51 +6,54 @@ module.exports = [
   //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
   // }],
 
-  'vuepress-plugin-baidu-autopush', // 百度自动推送
+  "vuepress-plugin-baidu-autopush", // 百度自动推送
 
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
   [
-    'thirdparty-search',
+    "thirdparty-search",
     {
       thirdparty: [
         // 可选，默认 []
         {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+          title: "在MDN中搜索",
+          frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
+          behindUrl: "", // 搜索链接的后面部分，可选，默认 ''
         },
         {
-          title: '在Runoob中搜索',
-          frontUrl: 'https://www.runoob.com/?s=',
+          title: "在Runoob中搜索",
+          frontUrl: "https://www.runoob.com/?s=",
         },
         {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
+          title: "在Vue API中搜索",
+          frontUrl: "https://cn.vuejs.org/v2/api/#",
         },
         {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q=',
+          title: "在Bing中搜索",
+          frontUrl: "https://cn.bing.com/search?q=",
         },
         {
-          title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Asweetheartjq.com%20',
+          title: "通过百度搜索本站的",
+          frontUrl: "https://www.baidu.com/s?wd=site%3Asweetheartjq.com%20",
         },
       ],
     },
   ],
 
   [
-    'one-click-copy',
+    "one-click-copy",
     {
       // 代码块复制按钮
-      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      copySelector: [
+        'div[class*="language-"] pre',
+        'div[class*="aside-code"] aside',
+      ], // String or Array
+      copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
       duration: 1000, // prompt message display time.
       showInMobile: false, // whether to display on the mobile side, default: false.
     },
   ],
   [
-    'demo-block',
+    "demo-block",
     {
       // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
       settings: {
@@ -64,47 +67,49 @@ module.exports = [
     },
   ],
   [
-    'vuepress-plugin-zooming', // 放大图片
+    "vuepress-plugin-zooming", // 放大图片
     {
-      selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+      selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
       options: {
-        bgColor: 'rgba(0,0,0,0.6)',
+        bgColor: "rgba(0,0,0,0.6)",
       },
     },
   ],
   [
-    'vuepress-plugin-baidu-tongji', // 百度统计
+    "vuepress-plugin-baidu-tongji", // 百度统计
     {
-      hm: '503f098e7e5b3a5b5d8c5fc2938af002',
+      hm: "503f098e7e5b3a5b5d8c5fc2938af002",
     },
   ],
   [
-    'vuepress-plugin-comment', // 评论
+    "vuepress-plugin-comment", // 评论
     {
-      choosen: 'gitalk',
+      choosen: "gitalk",
       options: {
-        clientID: '97cc3800172983cd3344',
-        clientSecret: '67a39c103de94fc00714a4573e01491dbf095297',
-        repo: 'myblog-gitalk-comment', // GitHub 仓库
-        owner: 'gujunling', // GitHub仓库所有者
-        admin: ['gujunling'], // 对仓库有写权限的人
+        clientID: "97cc3800172983cd3344", //  gujunling.github.io
+        clientSecret: "67a39c103de94fc00714a4573e01491dbf095297", //  gujunling.github.io
+        // 不同的域名需要更换不同的评论系统的id
+        // clientID: "1229f90ccdd99b9bea84",     //  sweetheartjq.cn
+        // clientSecret: "5d8699282be89aeded2b4cf41d29dda9c0fcd83f",  //  sweetheartjq.cn
+        repo: "myblog-gitalk-comment", // GitHub 仓库
+        owner: "gujunling", // GitHub仓库所有者
+        admin: ["gujunling"], // 对仓库有写权限的人
         // distractionFreeMode: true,
-        pagerDirection: 'last', // 'first'正序 | 'last'倒序
-        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-        body:
-          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+        pagerDirection: "last", // 'first'正序 | 'last'倒序
+        id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
+        title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
+        labels: ["Gitalk", "Comment"], // GitHub issue 的标签
+        body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
       },
     },
   ],
   [
-    '@vuepress/last-updated', // "上次更新"时间格式
+    "@vuepress/last-updated", // "上次更新"时间格式
     {
       transformer: (timestamp, lang) => {
-        const dayjs = require('dayjs') // https://day.js.org/
-        return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        const dayjs = require("dayjs"); // https://day.js.org/
+        return dayjs(timestamp).format("YYYY/MM/DD, HH:mm:ss");
       },
     },
   ],
-]
+];
