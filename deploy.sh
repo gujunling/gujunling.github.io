4edddf17b3602c0a1a193690827a0b82143bd1a7
gujunling <gujunq1998@163.com>
2023-12-26 18:29:08 +0800

    feat: 更新主题并修改配置文件及文档添加作者相关信息及部分文档问题修复

--- deploy.sh
+++ deploy.sh
@@ -3,30 +3,25 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+githubUrl=`git@github.com:gujunling/gujunling.github.io.git` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=`来自github actions的自动部署`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd $dist_path
 
-# deploy to github
-# echo 'gujunling.github.io' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:gujunling/gujunling.github.io.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://gujunling:${GITHUB_TOKEN}@github.com/gujunling/gujunling.github.io.git
-  git config --global user.name "gujunling"
-  git config --global user.email "gujunq1998@163.com"
-fi
 git init
 git remote add origin git@github.com:gujunling/gujunling.github.io.git
 git branch -M main
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl main:gh-pages # 推送到github
+git commit -m "deploy, ${commit_info}"
+git push -f $githubUrl main:$push_branch # 推送到github
 
 
 cd - # 退回开始所在目录
-rm -rf docs/.vuepress/dist
+rm -rf $dist_path
